a0809df5b00f4099259941f43f7342fd26cc3008
tzwenn <tzwenn@dfc2a060-1251-0410-9d7c-ab5543f5473a>
2008-08-31 16:34:07 +0000

    mkinitrd is now GPLv3
    git-svn-id: svn://svn.berlios.de/nupkux/trunk@22 dfc2a060-1251-0410-9d7c-ab5543f5473a

--- tools/mkinitrd/main.c
+++ tools/mkinitrd/main.c
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2008 Sven Köhler
+ *
+ *  This file is part of Nupkux.
+ *
+ *  Nupkux is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Nupkux is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Nupkux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -35,7 +54,7 @@ typedef struct _initrd_folder_entry
 {
 	UINT inode;
 	char filename[INITRD_FILENAME_LEN];
-	
+
 } initrd_folder_entry;
 
 UINT tmp_inode = 0;
@@ -53,7 +72,7 @@ UINT count_of_inodes(char *directory, int recursive)
 	DIR *pDIR;
 	struct dirent *pDirEnt;
 	char filename[255];
-	
+
 	pDIR=opendir(directory);
 	if (!pDIR) return -1;
 	pDirEnt=readdir(pDIR);
@@ -78,18 +97,18 @@ UINT count_of_inodes(char *directory, int recursive)
 UCHAR *expand_initrd(UINT diff)
 {
 	UINT oldsize = filelen;
-	
+
 	filelen+=diff;
 	filebuf=realloc(filebuf,filelen);
 	inodes=(initrd_inode *)((UINT)filebuf+sizeof(initrd_header));
-	
+
 	return (UCHAR *)(((UINT) filebuf)+oldsize);
 }
 
 void createinode(char *filename, initrd_inode *inode, UCHAR d_type)
 {
 	struct stat infobuf;
-	
+
 	stat(filename,&infobuf);
 	inode->inode=tmp_inode++;
 	inode->mode=infobuf.st_mode;
@@ -115,47 +134,47 @@ UINT savefile(char *filename, UCHAR d_type,UINT parent_inode)
 {
 	if (d_type==DT_DIR)
 		return savedir(filename,parent_inode);
-	
+
 	initrd_inode inode;
 	UCHAR *data;
 	FILE *f;
-	
+
 	createinode(filename,&inode,FS_FILE);
 	fprintf(stderr,"\tOpen file %s\n",filename);
 	header.entries++;
 	data=expand_initrd(inode.size);
-	
+
 	f=fopen(filename,"r");
 	fread(data,inode.size,1,f);
 	fclose(f);
-	
+
 	inodes[inode.inode]=inode;
 	return inode.inode;
 }
 
 UINT savedir(char *directory, UINT parent_inode)
-{	
+{
 	initrd_inode inode;
 	initrd_folder_entry *entries;
 	UINT tmp, i = HARDLINKS;
 	DIR *pDIR;
 	struct dirent *pDirEnt;
 	char filename[255];
-	
+
 	header.entries++;
 	createinode(directory,&inode,FS_DIRECTORY);
 	fprintf(stderr,"Enter directory %s (inode %d)\n",directory,inode.inode);
 	inode.size=sizeof(initrd_folder_entry)*(count_of_inodes(directory,0)+HARDLINKS);
 	entries=(initrd_folder_entry *)expand_initrd(inode.size);
-	
+
 	entries[0].inode=inode.inode;
 	strcpy(entries[0].filename,".");	//HARDLINK auf mich selbst
 	header.entries++;
-	
+
 	entries[1].inode=(parent_inode==-1)?inode.inode:parent_inode; //Wenn Wurzelknoten
 	strcpy(entries[1].filename,"..");	//HARDLINK auf Elternknoten
 	header.entries++;
-	
+
 	pDIR=opendir(directory);
 	if (!pDIR) return -1;
 	pDirEnt=readdir(pDIR);
@@ -174,7 +193,7 @@ UINT savedir(char *directory, UINT parent_inode)
 		pDirEnt=readdir(pDIR);
 	}
 	closedir(pDIR);
-	
+
 	inodes[inode.inode]=inode;
 	return inode.inode;
 }
@@ -184,8 +203,8 @@ int main(int argc, char* argv[])
 	char dirname[256];
 	UINT len;
 	FILE *f = stdout;
-	
-	
+
+
 	if (argc<2) {
 		fprintf(stderr,"Please specify directory.\n");
 		return 1;
